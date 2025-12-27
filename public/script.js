3cd6b9002a3ec0a6b29c6f12b3bb41023c69cf44
yuvraj singh <yuvrajsingghhh@gmail.com>
2025-12-27 09:46:10 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,23 +1,34 @@
-const socket = io();
+const socket = io(); // ngrok / same server
 
 const messages = document.getElementById("messages");
 const msgInput = document.getElementById("msg");
 
 function sendMsg() {
-  if (msgInput.value.trim() === "") return;
+  const text = msgInput.value.trim();
+  if (!text) return;
 
-  socket.emit("message", msgInput.value);
-  addMessage("Me: " + msgInput.value);
+  addMessage(text, "me");
+  socket.emit("message", text);
+
   msgInput.value = "";
 }
 
-socket.on("message", data => {
-  addMessage("Friend: " + data);
+socket.on("message", text => {
+  addMessage(text, "friend");
 });
 
-function addMessage(text) {
-  const p = document.createElement("p");
-  p.innerText = text;
-  messages.appendChild(p);
+function addMessage(text, type) {
+  const div = document.createElement("div");
+  div.classList.add("msg-bubble");
+
+  if (type === "me") {
+    div.classList.add("msg-me");
+  } else {
+    div.classList.add("msg-friend");
+  }
+
+  div.innerText = text;
+  messages.appendChild(div);
+
   messages.scrollTop = messages.scrollHeight;
 }
