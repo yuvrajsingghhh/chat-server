2f8270347078f1fa37639dc0d6e6c601aaa4309f
yuvraj singh <yuvrajsingghhh@gmail.com>
2025-12-27 09:47:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
 
-// UI serve karo
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",   // GitHub Pages / ngrok allowed
+    methods: ["GET", "POST"]
+  }
+});
+
+// Serve UI
 app.use(express.static("public"));
 
-// Socket.IO chat
 io.on("connection", socket => {
   console.log("User connected:", socket.id);
 
-  socket.on("message", msg => {
-    console.log("Received:", msg);
-    socket.broadcast.emit("message", msg);
+  // Message event
+  socket.on("message", data => {
+    socket.broadcast.emit("message", data);
+  });
+
+  // Seen event
+  socket.on("seen", () => {
+    socket.broadcast.emit("seen");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
 http.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
-
